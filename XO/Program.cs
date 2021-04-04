1a14e4f1aafa86af0d806e4f8d268af36f65b36d
dnbnero <dnbneroph@gmail.com>
2021-04-04 11:15:57 +0300

    Changed from if to switch in main cycle. Now you can play billion of times without re-run. Added tabs between numbers.

--- XO/Program.cs
+++ XO/Program.cs
@@ -7,69 +7,100 @@ int vyb = 0;
 char hod = 'X';
 char sledhod = 'O';
 char temp = '0';
+int pob = 0;
+int count_x = 0;
+int count_o = 0;
 
 while (true)
 {
-    VyvPole(pole1, pole2, pole3);
-    Hod(hod);
-    vyb = Convert.ToInt32(Console.ReadLine());
-    if (vyb == 1)
-        pole3[0] = hod;
-    else if (vyb == 2)
-        pole3[1] = hod;
-    else if (vyb == 3)
-        pole3[2] = hod;
-    else if (vyb == 4)
-        pole2[0] = hod;
-    else if (vyb == 5)
-        pole2[1] = hod;
-    else if (vyb == 6)
-        pole2[2] = hod;
-    else if (vyb == 7)
-        pole1[0] = hod;
-    else if (vyb == 8)
-        pole1[1] = hod;
-    else if (vyb == 9)
-        pole1[2] = hod;
+    while (true)
+    {
+        VyvPole(pole1, pole2, pole3);
+        Hod(hod);
+        vyb = Convert.ToInt32(Console.ReadLine());
+        switch (vyb)
+        {
+            case 1:
+                pole3[0] = hod;
+                break;
+            case 2:
+                pole3[1] = hod;
+                break;
+            case 3:
+                pole3[2] = hod;
+                break;
+            case 4:
+                pole2[0] = hod;
+                break;
+            case 5:
+                pole2[1] = hod;
+                break;
+            case 6:
+                pole2[2] = hod;
+                break;
+            case 7:
+                pole1[0] = hod;
+                break;
+            case 8:
+                pole1[1] = hod;
+                break;
+            case 9:
+                pole1[2] = hod;
+                break;
+            default:
+                Console.WriteLine("Error");
+                Console.ReadKey();
+                break;
+        }
+        pob = Pobeda(pole1, pole2, pole3, hod);
+        if (pob == 1)
+            break;
+        temp = hod;
+        hod = sledhod;
+        sledhod = temp;
+    }
+
+    Console.WriteLine("Выиграли " + hod + "!");
+    if (hod == 'X')
+    {
+        count_x++;
+        Console.WriteLine("X" + " выиграли " + count_x + " раз");
+    }
     else
-        Console.WriteLine("Error");
-        Console.ReadKey();
-
-
-    if ((pole1[0] == hod & pole1[1] == hod & pole1[2] == hod) ^ (pole2[0] == hod & pole2[1] == hod & pole2[2] == hod) ^ (pole3[0] == hod & pole3[1] == hod & pole3[2] == hod))
     {
-        break;
+        count_o++;
+        Console.WriteLine("X" + " выиграли " + count_x + " раз");
     }
-    if ((pole1[0] == hod & pole2[0] == hod & pole3[0] == hod) ^ (pole1[1] == hod & pole2[1] == hod & pole3[1] == hod) ^ (pole1[2] == hod & pole2[2] == hod & pole3[2] == hod))
-    {
-        break;
-    }
-    if ((pole1[0] == hod & pole2[1] == hod & pole3[2] == hod) ^ (pole1[2] == hod & pole2[1] == hod & pole3[0] == hod))
-    {
-        break;
-    }
-    temp = hod;
-    hod = sledhod;
-    sledhod = temp; 
+    Console.ReadKey();
+    pole1[0] = '7'; pole1[1] = '8'; pole1[2] = '9';
+    pole2[0] = '4'; pole2[1] = '5'; pole2[2] = '6';
+    pole3[0] = '1'; pole3[1] = '2'; pole3[2] = '3';
 }
-
-Console.WriteLine("Выиграли " + hod + "!");
-Console.ReadKey();
 
 static void VyvPole(char[] pole1, char[] pole2, char[] pole3)
 {
     Console.Clear();
     foreach (char i in pole1)
-        Console.Write(i + " ");
-    Console.Write("\n");
+        Console.Write(i + "\t ");
+    Console.Write("\n\n\n\n");
     foreach (char i in pole2)
-        Console.Write(i + " ");
-    Console.Write("\n");
+        Console.Write(i + "\t ");
+    Console.Write("\n\n\n\n");
     foreach (char i in pole3)
-        Console.Write(i + " ");
-    Console.Write("\n");
+        Console.Write(i + "\t ");
+    Console.Write("\n\n\n\n");
 }
 static void Hod(char hod)
 {
     Console.WriteLine("Ходят " + hod);
+}
+static int Pobeda(char[] pole1, char[] pole2, char[] pole3, char hod)
+{
+    if ((pole1[0] == hod & pole1[1] == hod & pole1[2] == hod) ^ (pole2[0] == hod & pole2[1] == hod & pole2[2] == hod) ^ (pole3[0] == hod & pole3[1] == hod & pole3[2] == hod))
+        return 1;
+    else if ((pole1[0] == hod & pole2[0] == hod & pole3[0] == hod) ^ (pole1[1] == hod & pole2[1] == hod & pole3[1] == hod) ^ (pole1[2] == hod & pole2[2] == hod & pole3[2] == hod))
+        return 1;
+    else if ((pole1[0] == hod & pole2[1] == hod & pole3[2] == hod) ^ (pole1[2] == hod & pole2[1] == hod & pole3[0] == hod))
+        return 1;
+    else return 0;
 }
